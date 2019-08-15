d14667f3cd30d92ae8c72b6e72f87c5d17f47ff0
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-15 02:22:41 +0100

    Created the atom CloseCollab

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import TitleCollab from "./components/atoms/TitleCollab.svelte";
+  import CloseCollab from "./components/atoms/CloseCollab.svelte";
 </script>
 
 <TitleCollab content="Faça login abaixo" />
+<CloseCollab />
---
+++ src/components/atoms/CloseCollab.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<style>
+  .close-collab {
+    position: relative;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 20px;
+    min-height: 20px;
+  }
+
+  .close-collab::before,
+  .close-collab::after {
+    content: "";
+    position: absolute;
+    width: 120%;
+    height: 4px;
+    border-radius: 2px;
+    background-color: #6c6c6c;
+  }
+
+  .close-collab::before {
+    transform: rotate(45deg);
+  }
+
+  .close-collab::after {
+    transform: rotate(-45deg);
+  }
+
+  .close-collab > .content {
+    display: block;
+    text-indent: -9999px;
+  }
+</style>
+
+<button class="close-collab">
+  <span class="content">Fechar</span>
+</button>
